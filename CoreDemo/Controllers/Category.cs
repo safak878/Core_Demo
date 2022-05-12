03f3986222a8ee9bf3b8427124f3da61dc665722
safak878 <58382184+safak878@users.noreply.github.com>
2022-05-12 15:57:55 +0300

    Add project files.

---
+++ CoreDemo/Controllers/Category.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.Entitiy_Framework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class Category : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EFCategoryRepository());
+        public IActionResult Index()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
